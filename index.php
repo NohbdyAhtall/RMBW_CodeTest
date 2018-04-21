7a28d22d7af829f6da199bb4b93fd6aff6859db5
Ryan McGill <ryanmcgillisaparadox@gmail.com>
2018-04-20 20:08:33 -0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>RM_BW_Form</title>
+	<style>
+	header {
+		background: #e3e3e3;
+		padding: 2em;
+		text-align: center;
+	}
+	</style>
+
+</head>
+<body>	
+	<header>
+		<h1>Hello, World</h1>
+	</header>
+</body>
+</html>
