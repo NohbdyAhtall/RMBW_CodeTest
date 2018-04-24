4fd0312759ffeb5017420c0ce37e6eebf7ba02f9
Ryan McGill <ryanmcgillisaparadox@gmail.com>
2018-04-23 20:38:32 -0700

    Fixed 'Form Disappearance'

--- formulator/resources/views/posts/index.blade.php
+++ formulator/resources/views/posts/index.blade.php
@@ -1,165 +1,98 @@
 @extends ('layouts.master')
 
-@section ('content')
+@section('form')
+<form method="POST" action="/posts">
 
-<main role="main">
+  @csrf
+  
+  <!-- Customer Information -->
+  <hr>
 
-      <section class="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </section>
+  <label for="customerInfo"><h3>Customer Information</h3></label>
+  <div class="form-group">
+    <input type="text" class="form-control" id="custFirstName" placeholder="First Name" name="custFirstName" required>
+  </div>
 
-      <div class="album py-5 bg-light">
-        <div class="container">
+  <div class="form-group">
+    <input type="text" class="form-control" id="custLastName" placeholder="Last Name" name="custLastName" required>
+  </div>
 
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
+  <div class="form-group">
+    <input type="text" class="form-control" id="custAddress1" placeholder="Address 1" name="custAddress1" required>
+  </div>
 
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
+  <div class="form-group">
+    <input type="text" class="form-control" id="custAddress2" placeholder="Address 2" name="custAddress2" required>
+  </div>
 
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="form-group">
+    <div class="row">
+      <div class="col-sm-3">
+        <input type="text" class="form-control" id="custCity" placeholder="City" name="custCity" required>
       </div>
+      <div class="col-sm-1" style="min-width: 76px;">
+        <input type="text" class="form-control" id="custState" placeholder="State" name="custState" required>
+      </div>
+      <div class="col-sm-2">
+        <input type="text" class="form-control" id="custZip" placeholder="Zip" name="custZip" required>
+      </div>
+    </div>
+  </div>
 
-    </main>
+  <div class="form-group">
+    <input type="text" class="form-control" id="custPhone" placeholder="Contact Phone" name="custPhone" required>
+  </div>
+  <div class="form-group">
+    <input type="email" class="form-control" id="custEmail" placeholder="Email Address" name="custEmail" required>
+  </div>
 
+
+  <!-- Company Information -->
+  <hr>
+
+  <label for="customerInfo"><h3>Company Information</h3></label>
+  <div class="form-group">
+    <input type="text" class="form-control" id="compName" placeholder="Company Name" name="compName" required>
+  </div>
+
+  <div class="form-group">
+    <input type="text" class="form-control" id="compAddress" placeholder="Company Address" name="compAddress" required>
+  </div>
+
+  <div class="form-group">
+    <div class="row">
+      <div class="col-sm-4">
+        <input type="text" class="form-control" id="compCity" placeholder="City" name="compCity" required>
+      </div>
+      <div class="col-sm-1" style="min-width: 76px;">
+        <input type="text" class="form-control" id="compState" placeholder="State" name="compState" required>
+      </div>
+      <div class="col-sm-2">
+        <input type="text" class="form-control" id="compZip" placeholder="Zip" name="compZip" required>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <input type="text" class="form-control" id="compPhone" placeholder="Contact Phone" name="compPhone" required>
+  </div>
+
+  <div class="form-group">
+    <label for="compInvoicePDF">Invoice File</label>
+    <!-- Validation  -->
+    <input type="file" id="compInvoicePDF" name="compInvoicePDF">
+    <p class="help-block"><i>Note: Only PDF files are allowed.</i></p>
+  </div>
+
+  <!-- Form Submission -->
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+<hr>
+@endsection
+
+@section ('adminLink')
+<h4>
+  <a href="/admin">Administrative Control Panel</a>
+</h4>
 @endsection
